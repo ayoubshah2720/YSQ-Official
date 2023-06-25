25cbe022d4ead555882e54f2c786d14cd526ab9c
Ayoub Shah <ayoubshah2720@gmail.com>
2023-06-25 10:47:14 +0500

    webview added

--- src/components/YoutubePlayer.js
+++ src/components/YoutubePlayer.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
-import YouTube from 'react-native-youtube';
+// import YouTube from 'react-native-youtube';
+import { WebView } from 'react-native-webview';
 
 const YouTubePlayer = ({ videoId }) => {
+  const youtubeUrl = `https://www.youtube.com/embed/${videoId}`;
   return (
     <View style={styles.container}>
       {/* <YouTube
@@ -10,13 +12,14 @@ const YouTubePlayer = ({ videoId }) => {
         videoId={videoId}
         style={styles.youtubePlayer}
       /> */}
-      <YouTube
+      {/* <YouTube
       apiKey="AIzaSyCNJUXsTrreXa3yDqfDwlcipQOiaRU324Y"
       videoId={videoId} // The YouTube video ID
       play // control playback of video with true/false
       fullscreen // control whether the video should play in fullscreen or inline
       loop // control whether the video should loop when ended
-      style={{ alignSelf: 'stretch', height: 300 }}/>
+      style={{ alignSelf: 'stretch', height: 300 }}/> */}
+      <WebView source={{ uri: youtubeUrl }} />
     </View>
   );
 };
